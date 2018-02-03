4dfe5fe70274e393969960712ff843333eda783f
collin_wong <collinwong53@gmail.com>
2018-02-03 15:36:43 -0800

    finished first prototype

--- intro1/src/App.js
+++ intro1/src/App.js
@@ -3,16 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  createLIs(targetObj){
+    const liContainer = [];
+    for(let key in targetObj){
+      liContainer.push(
+        <li><span className="label">{key}</span>{this.person[key]}</li>
+      );
+    }
+    return liContainer;
+  }
   render() {
+    this.person = {
+      'Name: ' :'Collin',
+      'Favorite Game: ' : 'Witcher3',
+      'Favorite Food: ' : 'Steak'
+    }
+    const {name, favoriteFood, favoriteGame} = this.person
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul>
+          {
+            this.createLIs(this.person)
+          }
+        </ul>
       </div>
     );
   }
