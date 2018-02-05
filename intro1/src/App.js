bc1084f96a4d15f3ebf6b535835d7fd9820e311a
collin_wong <collinwong53@gmail.com>
2018-02-05 00:10:04 -0800

    finished example

--- intro1/src/App.js
+++ intro1/src/App.js
@@ -3,29 +3,52 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  createLIs(targetObj){
-    const liContainer = [];
+  createProfiles(targetObj){
+    const profileContainer = [];
     for(let key in targetObj){
-      liContainer.push(
-        <li><span className="label">{key}</span>{this.person[key]}</li>
-      );
+      profileContainer.push(
+        <div className="person">
+          <div className="top_container">
+            <img className="faceshot" src={targetObj[key].headshot} alt="face shot"/>
+            <div className="details">{targetObj[key].name}</div>
+            <div className="details">{targetObj[key].age}</div>
+            <div className="details">{targetObj[key].occupation}</div>
+          </div>
+          <div className="bottom_container">{targetObj[key].description}</div>
+        </div>
+      )
     }
-    return liContainer;
+    return profileContainer;
   }
   render() {
-    this.person = {
-      'Name: ' :'Collin',
-      'Favorite Game: ' : 'Witcher3',
-      'Favorite Food: ' : 'Steak'
-    }
-    const {name, favoriteFood, favoriteGame} = this.person
+    const people = [
+      {
+        name: 'Jack',
+        age: 29,
+        occupation: 'florist',
+        headshot: 'https://cdn.goodgallery.com/3e6de05c-fdc8-4382-8ce7-abe2bec51aa9/s/0400/26dvkdld/manhattan-headshots.jpg',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, ipsa.'
+      },
+      {
+        name: 'Sally',
+        age: 32,
+        occupation: 'Dictator',
+        headshot: 'https://www.organicheadshots.com/images/headshot06-thumb.jpg',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, ipsa.'
+      },
+      {
+        name: 'Zarkon',
+        age:102,
+        occupation: 'Hypnotist',
+        headshot: 'https://www.wasiofaces.com/wp-content/uploads/San-Diego-Headshots-Photography-041.jpg',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, ipsa.'
+      }
+    ]
     return (
       <div className="App">
-        <ul>
           {
-            this.createLIs(this.person)
+            this.createProfiles(people)
           }
-        </ul>
       </div>
     );
   }
