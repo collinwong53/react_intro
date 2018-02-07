5172f3562a9ef1e58ba1635bf6fa45b034f6557e
collin_wong <collinwong53@gmail.com>
2018-02-06 20:46:05 -0800

    started stopwatch

--- intro1/src/App.js
+++ intro1/src/App.js
@@ -2,11 +2,23 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const DigitalClock = () => {
+  const time = new Date();
+  return(
+    <div className="clock">
+      <span>{time.getHours()}</span><span className="colon">:</span> 
+      <span>{time.getMinutes()}</span><span className="colon">:</span> 
+      <span>{time.getSeconds()}</span> 
+    </div>
+  );
+}
+
+class DigitalClockClass
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <DigitalClock />
       </div>
     );
   }
